1b2863d7d4f197a58844d614f5f7061a6f8ef7d0
Rose Wachuka <67165120+ro61zzy@users.noreply.github.com>
2023-01-14 13:29:33 +0300

    updated the landing page

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CreatePost from "./Components/CreatePost/CreatePost";
+
+const App = () => {
+  return (
+    <div>
+      <CreatePost />
+    </div>
+  );
+};
+
+export default App;
