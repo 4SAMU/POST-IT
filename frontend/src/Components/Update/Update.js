d3abe2385f637e5fa558d256c5cb0ef1ea3c5efe
ronkips <kipshillash@gmail.com>
2023-01-20 18:54:00 +0300

    update

--- frontend/src/Components/Update/Update.js
+++ frontend/src/Components/Update/Update.js
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import THeader from "../THeader/THeader";
+import "./Update.css";
 const Update = () => {
-  return (
-    <div>Update</div>
-  )
-}
+//   const [selectedFile, setSelectedFile] = useState(null);
+//   const [fileImage, setFile] = useState();
+//   const [formParams, updateFormParams] = useState({
+//     name: "",
+//     bio: ""
+//   });
+//   function inputFileHandler(e) {
+//     setSelectedFile(e.target.files[0]);
+//     setFile(URL.createObjectURL(e.target.files[0]));
+//   }
 
-export default Update
+  return (
+    <div className="updateA">
+      <THeader />
+      <div className="heady">Update yor Profile</div>
+      <div className="labeln">Upload Photo</div>
+      <div className="labeln1">Add Name</div>
+      <input type="text" className="txtarea1"></input>
+      <div className="labeln2">Add Bio </div>
+      <input type="text" className="txtarea2"></input>
+      <button className="updateButton">Update</button>
+    </div>
+  );
+};
+
+export default Update;
